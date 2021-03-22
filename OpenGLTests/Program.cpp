0b24744c2b95fec05d6a2f85a5fb5196bd5a8d3b
harshavb <48032800+harshavb@users.noreply.github.com>
2021-03-22 08:51:55 -0700

    Change OpenGL version from 4.4 to 4.6

--- OpenGLTests/Program.cpp
+++ OpenGLTests/Program.cpp
@@ -19,7 +19,7 @@ int main()
 {
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
